9af4347e295180c7031c7497820529c12169f505
youda <youda0518@yahoo.co.jp>
2024-09-25 13:18:55 +0900

    最低限の機能完成

--- download.php
+++ download.php
@@ -1,10 +1,18 @@
 <?php
+require 'vendor/autoload.php';
+
+$parsedown = new Parsedown();
+
 // POSTリクエストからパラメータを取得
 $editorContent = $_POST['editorContent'];
+$htmlString = $parsedown->text($editorContent);
 $action = $_GET['action'];
 
 if ($action == 'download') {
     header('Content-Type: text/markdown');
-    header('Content-Disposition: attachment; filename="download.md"');
-    echo $editorContent;
+    header('Content-Disposition: attachment; filename="download.html"');
+    echo $htmlString;
+} else {
+    header('Content-Type: text/html');
+    echo $htmlString;
 }
